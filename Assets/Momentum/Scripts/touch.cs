6820949643331057bef344c53fb28a161e5ce8be
jjaychen <jjaychen1e@gmail.com>
2018-12-14 19:13:00 +0800

    调整了目录结构，小球碰撞的实验文件夹目录为Momentum（动量）

---
+++ Assets/Momentum/Scripts/touch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour {
+    private int time = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(Input.GetMouseButtonDown(0) && Input.GetTouch(0).phase==TouchPhase.Began)
+        //{
+        //    DestroyImmediate(this.gameObject);
+        //}
+        //if (time < 1) time++;
+
+
+        if (Input.GetMouseButtonDown(0))//鼠标左键点下
+        {
+            //住摄像机向鼠标位置发射射线  
+            LayerMask mask = 1 << LayerMask.NameToLayer("Sphere");
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mHit;
+            //射线检验  
+            if (Physics.Raycast(mRay, out mHit, 20f, mask.value))
+            {
+                if (mHit.collider.gameObject == this.gameObject)// && time >= 1
+                {
+                    DestroyImmediate(this.gameObject);
+                }
+            }
+        }
+
+	}
+}
---
+++ Assets/TempTest/colt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colt : MonoBehaviour {
+    public GameObject obj1;
+    public GameObject obj2;
+    public GameObject obj;
+    private GameObject[] currentObj = new GameObject[2];
+    // Use this for initialization
+    void Start () {
+        Vector3 direction = obj2.transform.position - obj1.transform.position;
+        //obj1.GetComponent<Rigidbody>().velocity = direction.normalized * 3f;
+        obj1.GetComponent<Rigidbody>().velocity = direction.normalized*0.5f;
+        obj2.GetComponent<Rigidbody>().velocity = -direction.normalized*0.5f;
+        //currentObj[0] = Instantiate(obj);
+        //currentObj[0].transform.position = obj1.transform.position;
+        //currentObj[0].transform.rotation = obj1.transform.rotation;
+
+        //currentObj[1] = Instantiate(obj);
+        //currentObj[1].transform.position = obj2.transform.position;
+        //currentObj[1].transform.rotation = obj2.transform.rotation;
+
+        //currentObj[0].GetComponent<Rigidbody>().mass = 0.01f;
+        //Vector3 direction = currentObj[1].transform.position - currentObj[0].transform.position;
+        //currentObj[0].GetComponent<Rigidbody>().velocity = direction.normalized * 3f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
