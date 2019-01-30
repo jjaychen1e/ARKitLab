743f632f2603c30b288de59120591b12628875f8
jjaychen <jjaychen1e@gmail.com>
2019-01-30 20:38:59 +0800

    实现了实像的显示，采用按钮的方式控制。
    通过公式计算实像的大小和位置，但是没有考虑小于一倍焦距的情况。
    通过按钮控制，将文本拆成两个控件达到更改文字不会抖动的效果。
    将模型逻辑结构重置，手动将AlphaF和Glass的中心调整到同一水平面上（通过添加两个空的GameObject作为Parent的方法）。

---
+++ Assets/Labs/ConvexLensImaging/Scripts/VirtualImageController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualImageController  : MonoBehaviour {
+    public GameObject Model, RF, Glass, VF;
+    public Text text;
+    public GameObject Button;
+    private const float focal = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //在拖动实现之后通过委托事件来实现，现在通过Update实现/
+        //float times = VF.transform.localScale.magnitude / RF.transform.localScale.magnitude;
+        float u = (Glass.transform.position - RF.transform.position).magnitude;
+        float scale = focal / (u - focal);
+        VF.transform.position = Glass.transform.position + (Glass.transform.position - RF.transform.position).normalized * u * scale;
+        VF.transform.localScale = RF.transform.localScale * scale;
+
+        if (Model.activeSelf) Button.SetActive(true);
+        else Button.SetActive(false);
+
+    }
+
+    public void ShowImageOrNot()
+    {
+        if (VF.activeSelf)
+        {
+            VF.SetActive(false);
+            text.text = "OFF";
+        }
+        else
+        {
+            VF.SetActive(true);
+            text.text = "ON ";
+        }
+    }
+}
