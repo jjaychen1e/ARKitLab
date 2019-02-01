ce41a9cdd481d1a3693cee072a06e8d5dfa0e0a2
jjaychen <jjaychen1e@gmail.com>
2019-02-01 21:10:11 +0800

    完成动态模糊
    线性模拟

--- Assets/Labs/ConvexLensImaging/Scripts/ScreenImageController.cs
+++ Assets/Labs/ConvexLensImaging/Scripts/ScreenImageController.cs
@@ -8,17 +8,13 @@ public class ScreenImageController : MonoBehaviour {
     // Use this for initialization
 	void Start () {
         focal = this.gameObject.GetComponent<VirtualImageController>().focal;
-        Camera3.GetComponent<CameraFilterPack_Blur_Movie>().FastFilter = 6;
-        Debug.Log(Camera3.GetComponent<CameraFilterPack_Blur_Movie>().FastFilter.ToString());
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //distance = (Center.transform.position - VF.transform.position).magnitude;
-        //if (distance > focal) degree = 8;
-        //else degree = 7 * distance/focal + 1;
-        //Debug.Log(degree.ToString());
-        //Camera3.GetComponent<CameraFilterPack_Blur_Movie>().FastFilter = (int)degree;
-        //Debug.Log(Camera3.GetComponent<CameraFilterPack_Blur_Movie>().FastFilter.ToString());
+        distance = (Center.transform.position - VF.transform.position).magnitude;
+        if (distance > 0.25f * focal) degree = 8;
+        else degree = 28 * distance/focal + 1;
+        Camera3.GetComponent<CameraFilterPack_Blur_Movie>().FastFilter = (int)degree;
     }
 }
