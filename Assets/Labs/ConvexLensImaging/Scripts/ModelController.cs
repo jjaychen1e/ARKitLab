8628a661af364b0d6cc5b029188ff2d5167bdcef
jjaychen <jjaychen1e@gmail.com>
2019-02-04 22:43:15 +0800

    将移动改为HitTest实现，物体不会脱离平面。打开了平面的图形。

--- Assets/Labs/ConvexLensImaging/Scripts/ModelController.cs
+++ Assets/Labs/ConvexLensImaging/Scripts/ModelController.cs
@@ -10,6 +10,7 @@ public class ModelController : MonoBehaviour {
     public GameObject GeneratePlane, FocusSquare;
 
     private bool flag;
+    private bool adjustable = true;
 
     public delegate void SetModelActiveHandler();
     public event SetModelActiveHandler SetModelActiveEvent;
@@ -18,6 +19,9 @@ public class ModelController : MonoBehaviour {
     public delegate void ResetHandler();
     public event ResetHandler ResetHandlerEvent;
 
+    Vector3 oldPosition1;
+    Vector3 oldPosition2;
+
     // Use this for initialization
     void Start () {
         ResetHandlerEvent += SetModelInactive;
@@ -25,7 +29,8 @@ public class ModelController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Model.activeSelf == false)
+        if (Model.activeSelf == false)
+        {
             if (Input.GetMouseButtonDown(0))
             {
                 var touch = Input.GetTouch(0);
@@ -38,7 +43,8 @@ public class ModelController : MonoBehaviour {
                         y = screenPos.y
                     };
                     List<ARHitTestResult> hitTestResults = UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest(point, ARHitTestResultType.ARHitTestResultTypeExistingPlane);
-                    if (flag == false && hitTestResults.Count > 0) { 
+                    if (flag == false && hitTestResults.Count > 0)
+                    {
                         UnityARCameraManager.CloseDetection();
                         flag = true;
                         GeneratePlane.GetComponent<FocusSquare>().enabled = false;
@@ -51,7 +57,68 @@ public class ModelController : MonoBehaviour {
                     SetModelActive();
                 }
             }
-    }
+        }else if(adjustable)
+        {
+            if (Input.touchCount == 2)
+            {
+                var touch1 = Input.GetTouch(0);
+                var touch2 = Input.GetTouch(1);
+                if(touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved)
+                {
+                    Vector3 screenPos = Camera.main.ScreenToViewportPoint((touch1.position+touch2.position)/2);
+                    ARPoint point = new ARPoint
+                    {
+                        x = screenPos.x,
+                        y = screenPos.y
+                    };
+                    List<ARHitTestResult> hitTestResults = UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest(point, ARHitTestResultType.ARHitTestResultTypeExistingPlane);
+                    Model.transform.position = UnityARMatrixOps.GetPosition(hitTestResults[hitTestResults.Count - 1].worldTransform);
+                    Model.transform.rotation = UnityARMatrixOps.GetRotation(hitTestResults[hitTestResults.Count - 1].worldTransform);
+                }
+                
+                //保证重新开始双指触摸记录的初始位置不是松开手时的位置，而是初始触摸的位置
+                if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)
+                {
+                    //记录初始位置
+                    oldPosition1 = Input.GetTouch(0).position;
+                    oldPosition2 = Input.GetTouch(1).position;
+                }
+
+                if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Moved)
+                {
+                    //计算出当前两点触摸点的位置
+                    var tempPosition1 = Input.GetTouch(0).position;
+                    var tempPosition2 = Input.GetTouch(1).position;
+
+                    float currentTouchDistance = Vector2.Distance(tempPosition1, tempPosition2);
+                    float lastTouchDistance = Vector2.Distance(oldPosition1, oldPosition2);
+
+                    //计算上次和这次双指触摸之间的距离差距
+                    //然后去更改摄像机的距离
+                    float distance = currentTouchDistance - lastTouchDistance;
+                    float scaleFactor = distance / 200f;
+                    Vector3 localScale = Model.transform.localScale;
+                    Vector3 scale = new Vector3(localScale.x + scaleFactor,
+                        localScale.y + scaleFactor,
+                        localScale.z + scaleFactor);
+                    //在什么情况下进行缩放
+                    if (scale.x >= 0.2f && scale.y >= 0.2f && scale.z >= 0.2f)
+                    {
+                        Model.transform.localScale = scale;
+                    }
+                    //备份上一次触摸点的位置，用于对比
+                    //也是松开手时的位置
+                    oldPosition1 = tempPosition1;
+                    oldPosition2 = tempPosition2;
+                }
+            }
+            else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Vector2 deltaPos = Input.GetTouch(0).deltaPosition;
+                Model.transform.Rotate(Vector3.down * deltaPos.x * 0.5f, Space.World);
+            }
+        }
+}
 
     void SetModelActive()
     {
@@ -68,5 +135,10 @@ public class ModelController : MonoBehaviour {
     public void  Reset()
     {
         if (ResetHandlerEvent != null) ResetHandlerEvent();
+    }
+
+    public void SetAdjustable()
+    {
+        adjustable = false;
     }
 }
