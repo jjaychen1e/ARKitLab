637c307bc5446f1647c3846d775e5c22bbac371d
mufanx <xmfnnj@outlook.com>
2019-02-27 21:18:40 +0800

    开始/动画

---
+++ Assets/Labs/MainScene/Scripts/LabCount.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabCount : MonoBehaviour {
+    public int count;
+    // Use this for initialization
+
+}
