aad55728f607320932dfb8e6b38ebc4f585825f1
HavenTong <43297210+HavenTong@users.noreply.github.com>
2019-03-04 21:56:34 +0800

    距离标签以及成像显示
    ObjectDistanceLabel, ImageDistanceLabel, ShowImage

---
+++ Assets/Labs/ConvexLensImaging/Scripts/ShowDistance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowDistance : MonoBehaviour {
+    public GameObject measure;
+    public GameObject maginifier;
+    public Text distance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float measureX = measure.GetComponent<Transform>().position.x;
+        float magnifierX = maginifier.GetComponent<Transform>().position.x;
+        distance.text = (100 * Mathf.Abs(magnifierX - measureX)).ToString("f1") + "cm";
+	}
+}
